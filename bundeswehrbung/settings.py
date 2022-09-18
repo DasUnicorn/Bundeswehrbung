9e545de9431c797d9a1d1b7638c2072bad7c3348
dasUnicorn <info@dasunicorn.de>
2022-09-18 18:12:43 +0200

    fix local settings import

--- bundeswehrbung/settings.py
+++ bundeswehrbung/settings.py
@@ -124,6 +124,6 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 try:
-    from settings_local import *
+    from bundeswehrbung.settings_local import *
 except ImportError:
     pass
