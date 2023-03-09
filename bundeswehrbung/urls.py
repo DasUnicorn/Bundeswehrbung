f227d0a6a9c724856532ba2e86cc8e488dc8732d
dasUnicorn <info@dasunicorn.de>
2023-03-09 18:48:35 +0100

    add data as a new site

--- bundeswehrbung/urls.py
+++ bundeswehrbung/urls.py
@@ -17,10 +17,12 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('wordsearch.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 
