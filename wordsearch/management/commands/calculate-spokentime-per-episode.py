f5b45e95b88985334a73f0304277f46e15cee927
dasUnicorn <info@dasunicorn.de>
2023-04-26 13:25:15 +0200

    add and update admin commands to update and import data

--- wordsearch/management/commands/calculate-spokentime-per-episode.py
+++ wordsearch/management/commands/calculate-spokentime-per-episode.py
@@ -1,11 +1,12 @@
-# Counting the words in each episode and writing them inti the database
+# Calculating the spoken time in each episode and writing it into the database
 
 import os
 from django.core.management.base import BaseCommand
 from wordsearch.models import Line, Episode
+import datetime
 
 class Command(BaseCommand):
-	help = 'Import the number of words for a number of episodes'
+	help = 'Import the spoken time got a number of episodes'
 
 	def add_arguments(self, parser):
 		parser.add_argument("ep", type=int)
@@ -13,15 +14,14 @@ class Command(BaseCommand):
 	def handle(self, *args, **options):
 		ep = options['ep']
 
-		for e in range(0,ep+1):
-			count = 0
+		for e in range(1,ep+1):
+			time = datetime.timedelta(0)
 			episode_lines = Line.objects.filter(episode = e)
 
 			for line in episode_lines:
-				words = line.text.split()
-				count = count + len(words)
+				spoken_time = line.end_time - line.start_time
+				time = time + spoken_time
 
-			print("Words in episode " + str(e) + ": " + str(count))
 			current_episode = Episode.objects.get(number= e)
-			current_episode.spoken_words = count
+			current_episode.spoken_time = time
 			current_episode.save()
